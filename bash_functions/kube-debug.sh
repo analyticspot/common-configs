f338ddd7c80ab80dd4d54590457285cf63ad6643
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2016-04-28 15:17:02 -0700

    Added comments.

--- bash_functions/kube-debug.sh
+++ bash_functions/kube-debug.sh
@@ -1,3 +1,8 @@
+# Running `kubectl run ...` in order to spin up our debug container is pretty common. But it's a bit of a pain
+# because you have to be sure that the generated deployment has a unique name, the command itself is pretty
+# long, and you have to remember to clean up the deployment when you're done. This adds a simple bash function
+# that generates a random name, spins up a debug container using that name and then, when you exit the 
+# attache container it cleans up the deployment and pod.
 kdbg() {
    rand_name=$(date | shasum | cut -c1-5)
    kubectl run $rand_name -i --tty --image=672129611065.dkr.ecr.us-west-2.amazonaws.com/debug:v2
